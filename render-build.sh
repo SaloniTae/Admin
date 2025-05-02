27918424a246af8c167ffb2e7b267c268d0aa83f
SaloniTae <98252350+SaloniTae@users.noreply.github.com>
2025-05-02 07:21:48 +0530

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 set -o errexit
 
-# tell Puppeteer to skip its own download
 export PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true
-# point to Render’s Chrome
 export PUPPETEER_EXECUTABLE_PATH=/opt/render/project/.render/chrome/opt/google/chrome
+
 
 # install only your Node deps
 npm install
