34695c6092d25151e17365f17d0b9148b6949d2c
SaloniTae <98252350+SaloniTae@users.noreply.github.com>
2025-05-02 06:52:41 +0530

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash
 set -o errexit
 
-npm install
-
-PUPPETEER_CACHE_DIR=/opt/render/.cache/puppeteer
-mkdir -p $PUPPETEER_CACHE_DIR
-
-npx puppeteer browsers install chrome
-
-# Persist between builds
-cp -R $PUPPETEER_CACHE_DIR /opt/render/project/src/.cache/puppeteer/chrome/
+# 1. Install your Node dependencies (CI is faster & clean)
+npm ci
